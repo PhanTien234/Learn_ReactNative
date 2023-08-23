a9b0833e1c9d9b5965180afe80845af808a36d90
PhanTien234 <minhtienp328@gmail.com>
2023-08-23 16:12:01 +0700

    Hello World use State

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
+  const [message, setMessage] = useState("Hello World with State");
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Space!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>{message}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
